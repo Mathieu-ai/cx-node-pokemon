548c7f01ff7dfae8c65695a4d913408adaddc4bb
Mathieu Lièvre <mathieu.lievre@efrei.net>
2020-11-27 11:18:27 +0100

    step 3

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const bodyParser = require("body-parser")
 const fs = require('fs')
 
-var file;
+var pokedex;
 fs.readFile('./pokedex-27112020.json', 'utf8' , (err, data) => {
   if (err) {
     console.error(err)
     return
   }
-  file=JSON.parse(data)
-  //console.log(JSON.stringify(file))
+  pokedex=JSON.parse(data)
+  //console.log(JSON.stringify(pokedex))
 })
 
 app.use(bodyParser.json())
@@ -23,15 +23,26 @@ app.get("/", function(request, response) {
 
 app.get("/pokemon", function(request, response) {
 
-  return response.send(JSON.stringify(file, null, 5));
+  return response.send(JSON.stringify(pokedex, null, 5));
 });
 
 app.get("/pokemon/:id", function(request, response) {
 
-  return response.send("<html> <body> <h1> Bonjour </h1> </body>");
+  const result = pokedex.filter(pokm => pokm.id == request.params.id);
+  return response.send(result)
+  //return response.send("<html> <body> <h1> Bonjour </h1> </body>");
 });
 
-
+app.post('/items', (req, res) => {
+  const item = {
+      id: File.length + 1,
+      name: req.body.name,
+      description: req.body.description,
+      type: req.File.type
+  }
+  file.push(item);
+  res.json(item);
+})
 
 
 app.listen(3000, function() {
